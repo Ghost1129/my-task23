9ae76060d534cd91823b5e0d81c956c8dac97a28
Devansh Lodhi <1129.gh0st@gmail.com>
2023-05-23 22:11:49 +0530

    init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,113 @@
+"use client"
+
 import Image from 'next/image'
+import {motion} from 'framer-motion'
+
+const data = [
+  {
+    title: 'AI + RPA Automation',
+    description: 'Future-Proof your business. Drive efficiency, profitability and deliver on customer experience',
+    image: '/assets/rp2.jpg',
+    buttontext: 'AI + RPA Automation'
+  },
+  {
+    title: 'Make Bolder Choices',
+    description: 'Digital focused strategies to realize market-changing ideas',
+    image: '/assets/p1.png',
+    buttontext: 'Build Better Apps'
+  },{
+    title: 'Innovate with Speed',
+    description: 'Create higher quality software, deliver on customer expectations and business goals',
+    image: '/assets/p2.jpg',
+    buttontext: 'DevOps'
+  },{
+    title: 'Embrace Cloud',
+    description: 'With Cloud-First accelerate innovation and optimize performance',
+    image: '/assets/p3.jpg',
+    buttontext: 'Cloud Services'
+  }
+]
+
+const ButtonVariants = {
+  rest : {
+    scale: 1,
+    transition: {
+      duration: 0.3,
+    }
+  },
+  hover : {
+    scale: 1.1,
+    transition: {
+      duration: 0.3,
+    }
+  }
+}
+
+const arrowVariants = {
+  rest : {
+    x: 0,
+    transition: {
+      duration: 0.3,
+    }
+  },
+  hover : {
+    x: 10,
+    transition: {
+      duration: 0.3,
+    }
+  }
+}
+const ContainerVariants = {
+  rest : {
+    opacity: 0,
+    y: 40,
+  },
+  hover : {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      delay: 0.2
+    }
+  }
+
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='m-6'>
+      <h4 className='mb-20 text-2xl font-bold'>Let&apos;s Collaborate</h4>
+      {data.map((item, index) => (
+        <motion.div 
+        initial='rest'
+        whileInView='hover'
+        variants={ContainerVariants}
+        key={index} className='flex justify-between'>
+        <div className='flex flex-col md:justify-between md:w-[600px]'>
+        <h2 className='mb-10 font-serif text-4xl font-thin leading-10 md:text-5xl md:mt-4'>{item.title}</h2>
+        <div className='relative w-[428px] h-[290px] md:hidden'>
+          <Image src={item.image} alt="AI + RPA" fill />
         </div>
-      </div>
+        <p className='my-6 font-serif text-xl font-medium md:-mt-16 '>{item.description}</p>
+        <motion.div
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        initial='rest'
+        whileHover='hover'
+        animate='rest'
+         className='flex items-center gap-4 mb-6 cursor-pointer w-fit'>
+          <motion.button
+          
+           className='text-lg font-bold md:text-xl md:text-black text-sky-600 hover:text-blue-500 md:hover:text-gray-700'>{item.buttontext}</motion.button>
+          <motion.div 
+          variants={arrowVariants}
+          className='relative w-8 h-4'><Image className='object-contain' src="/assets/arrow.svg" fill alt='arrow' /></motion.div>
+        </motion.div>
+        </div>
+        <div className='relative hidden md:block mr-12 w-[590px] h-[360px]'>
+          <Image src={item.image} alt="AI + RPA" fill />
+        </div>
+      </motion.div>
+      ))}
+    </div>
   )
 }
